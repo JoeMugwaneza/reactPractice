1bd054549bdb21fe383e8001de90f91d19f82f35
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-07-04 13:44:56 +0200

    work on first react project

---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Main(){
+    return (
+        <div className='main-container'>
+            <h1>Fun Facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100k stars on Github</li>
+                <li>Is maintained by Facebook</li>
+                <li>Power thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </div>
+
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar(){
+    return (
+        <nav className='react-bar'>
+                <img src="../logo192.png" alt='logo' className='nav--image'></img>
+                <h3>ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
+        </nav>
+    )
+}
