9c0fe7cb5be14c212aebf07d90a7372e2c302f5a
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-07-05 06:06:17 +0200

    working on counter project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
-import {Main, Count} from './components/Main';
-import Navbar from './components/Navbar'
+// import {Main, Count} from './components/Main';
+// import Navbar from './components/Navbar'
+import Counter from './components/counter';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Count />
+      
+      {/* FOR MY EXISTING REACT PRACTICE */}
+      {/* <Navbar /> */}
+      {/* <Count /> */}
+
+
+      <Counter />
     </div>
   );
 }
