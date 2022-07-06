d72fd245ba66c56fcadb891c60acdc42b3f7be89
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-07-06 05:20:50 +0200

    able to create class method and render list in react

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,30 +2,31 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0
-    }
+        count: 0, 
+        tags: ["tag1", "tag2", "tag3"]
+        
+    };
 
-    styles = {
-        backgroundColor: "green",
-        fontWeight: 50
+    render() {         
+        return (
+            <React.Fragment>
+                <h1 className={this.getBadgeClasses()}>{this.formatCount()}</h1>
+                <button className='btn btn-secondary btn-sm'>Increment</button>
+                <ul>{this.state.tags.map(tag => <li key={tag}>{ tag}</li>) }</ul>
+            </React.Fragment>
+        )
     }
-
-    render() { 
-        let classes = "badge m-2 badge-"
-        classes += this.state.count === 0 ? "Warning" : "primary";
-
-        return ( 
-        <React.Fragment>
-                <span className={classes}>{this.formatCount() }</span>
-            <button className='btn btn-secondary btn-sm'>Increment</button>
-        </React.Fragment>
-        );
-    }
+    
+    getBadgeClasses() {
+        let classes = "badge m-2 bg-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes
+    };
 
     formatCount() {
         const {count} = this.state
-        return count === 0 ? <h4>Zero</h4>: count
-    }
+        return count === 0 ? "Zero" : count;
+    };
 }
 
 export default Counter;
