7724aceb56896c1a08160e85e4bed91f7543108d
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-07-09 05:57:06 +0200

    multi components in sync

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import './App.css';
 // import {Main, Count} from './components/Main';
-// import Navbar from './components/Navbar'
+import Navbar from './components/Navbar'
 import Counters from './components/counters';
 
 function App() {
   return (
     <React.Fragment>
       {/* FOR MY EXISTING REACT PRACTICE */}
-      {/* <Navbar /> */}
+      <Navbar />
       {/* <Count /> */}
-
-      <Counters />
+      <main className='container'>
+        <Counters />
+      </main>
     </React.Fragment>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <nav className="navbar navbar-light bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Navbar</a>
+                </div>
+            </nav>);
+    }
+}
+ 
+export default NavBar;
